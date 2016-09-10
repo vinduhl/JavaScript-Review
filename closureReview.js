c8dac25c2a7b07731313740514cc3371d4b15b1b
Wendell Valdez <vinduhl@gmail.com>
2016-09-10 12:02:21 -0500

    added answers

--- closureReview.js
+++ closureReview.js
@@ -6,11 +6,12 @@ var callFriend = function(){
   return callF;
 };
 
-//Above you're given a callFriend function that returns another function. 
+//Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
   //code here
-
+var call = callFriend();
+console.log(call("435-215-9248"));
 
 
 /*
@@ -20,3 +21,27 @@ Write a function that accepts a function as it's first argument and returns a ne
 Once completed, add a second arguments that allows the function to be executed N number of times. After the function has been called N number of times, console.log('STAHHP');
 
 */
+
+var myFunction = function (callback, maxExecutionTimes) {
+
+  var executionCount = 0;
+
+  var myOtherFunction = function() {
+    executionCount++;
+    if(executionCount <= maxExecutionTimes) {
+      callback(executionCount);
+    } else {
+      console.log("STAHHP");
+    }
+  }
+
+  return myOtherFunction;
+}
+
+var functionToExecute = myFunction( function(executionCount) {
+  console.log("executionCount is " + executionCount)
+}, 4);
+
+for(var i = 0; i < 10; i++) {
+  functionToExecute();
+}
