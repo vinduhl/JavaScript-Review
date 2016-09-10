b93c28f72e9f5278ec62328c752b6a7e23ed60f9
Wendell Valdez <vinduhl@gmail.com>
2016-09-10 11:21:03 -0500

    added answers

--- callbackReview.js
+++ callbackReview.js
@@ -1,6 +1,8 @@
 /* Declare and Define the functions here that will make the function calls below work properly */
 
-
+function first(nameArray, callback) {
+  callback(nameArray[0]);
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 first(names, function(firstName){
@@ -12,7 +14,9 @@ first(names, function(firstName){
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
+function last(nameArray, callback) {
+  callback(nameArray[nameArray.length - 1]);
+}
 
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -27,6 +31,9 @@ last(names, function(lastName){
 
 //have the contains function return a boolean value for if the name is in the array or not.
 
+function contains(name, nameArray, callback) {
+  callback(nameArray.indexOf(name) !== -1);
+}
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 contains('Colt', names, function(yes){
@@ -42,21 +49,35 @@ contains('Colt', names, function(yes){
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
+function map(numbersArray, callback) {
+  for(var i = 0; i < numbersArray.length; i++) {
+    numbersArray[i] = callback(numbersArray[i]);
+  }
 
+  return numbersArray;
+}
 
 
 var numbers = [1,2,3,4,5];
 //Produces a new array of values by mapping each value in list through a transformation function
-map(numbers, function(num){
+console.log(map(numbers, function(num){
   return num * 2; //returns an array of [2,4,6,8,10]
-});
+}));
 
 
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
+function uniq(namesArray, callback) {
+  for(var i = namesArray.length - 1 ; i > 0; i--) {
+    if(i !== namesArray.indexOf(namesArray[i])) {
+      namesArray.splice(i, 1);
+    }
+  }
+  callback(namesArray);
+  return namesArray;
+}
 
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -69,12 +90,16 @@ uniq(names, function(uniqArr){
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
+function each(namesArray, callback) {
+  for(var i = 0; i < namesArray.length; i++) {
+    callback(namesArray[i], i);
+  }
+}
 
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 each(names, function(item, indice){
-  console.log('The item in the ' + indice + 'position is ' + item)
+  console.log('The item in the ' + indice + ' position is ' + item)
 });
 
 
@@ -82,7 +107,15 @@ each(names, function(item, indice){
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
-
+function getUserById(id, usersArray, callback) {
+  for(var i = 0; i < usersArray.length; i++) {
+    var user = usersArray[i];
+    if(user.id === id) {
+      callback(user);
+      break;
+    }
+  }
+}
 
 
 var users = [
@@ -106,7 +139,7 @@ var users = [
   },
 ];
 getUserById('16t', users, function(user){
-  console.log('The user with the id 16t has the email of ' + user.email + 'the name of ' + user.name + ' and the address of ' + user.address); 
+  console.log('The user with the id 16t has the email of ' + user.email + 'the name of ' + user.name + ' and the address of ' + user.address);
 });
 
 
@@ -115,10 +148,16 @@ getUserById('16t', users, function(user){
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
+function find(numbersArray, callback) {
+  for(var i = 0; i < numbersArray.length; i++) {
+    if(callback(numbersArray[i])) {
+      return numbersArray[i];
+    }
+  }
+}
 
-
-//Looks through each value in the list, returning the first one that passes a truth test 
+//Looks through each value in the list, returning the first one that passes a truth test
 var numbers  = [1, 2, 3, 4, 5, 6];
-find(numbers, function(num){ 
+console.log(find(numbers, function(num){
   return num % 2 == 0; //should return 2
-})
+}));
